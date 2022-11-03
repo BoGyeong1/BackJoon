88ceb53ad31b62fb836818e07c7b1064c9d37fbc
BoGyeong1 <wls6590@gmail.com>
2022-11-03 16:13:31 +0900

    UP plus

--- src/AddAB.java
+++ src/AddAB.java
@@ -1,8 +1,26 @@
 import java.util.Scanner;
 
+// -문제파악
+// 두 정수 a와 b를 입력받은 다음 a+b를 출력
+// 입력값은 (0<A, B<10) 따라야한다
+// -유추파악
+// 두 정수를 입력해 합을 출력
+// -주요단어 영문이름 선정
+// 입력 input
+// 정수 decimal
+// 출력 output
+// 합 plus
+
 //문제 : 두 정수 A와 B를 입력받은 다음, A+B를 출력하는 프로그램을 작성하시오.
 //refer : https://www.acmicpc.net/submit/1000/48316653
 public class AddAB {
+
+    // 밑의 input_first, second랑 다른 변수에요~ 영역이 달라여.. 지역변수?
+
+    public int plus(int input_first, int input_second) {
+        int output_plus = input_first + input_second;
+        return output_plus;
+    }
 
     public static void main(String[] args) {
 
@@ -23,7 +41,8 @@ public class AddAB {
         int input_second = scanner.nextInt();
 
         // 처리
-        int output_plus = input_first + input_second;
+        AddAB addAb = new AddAB();
+        int output_plus = addAb.plus(input_first, input_second);
 
         // 출력 : 첫째 줄에 A+B를 출력한다.
         System.out.println(output_plus);
@@ -32,14 +51,3 @@ public class AddAB {
     }
 
 }
-
-// -문제파악
-// 두 정수 a와 b를 입력받은 다음 a+b를 출력
-// 입력값은 (0<A, B<10) 따라야한다
-// -유추파악
-// 두 정수를 입력해 합을 출력
-// -주요단어 영문이름 선정
-// 입력 input
-// 정수 decimal
-// 출력 output
-// 합 plus
--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,69 @@
+import java.util.Scanner;
+
+// -문제파악
+// 두 정수 a와 b를 입력받은 다음 a+b를 출력
+// 입력값은 (0<A, B<10) 따라야한다
+// 테스트 케이스 개수 T가 처음에 주어진다.
+// -유추파악
+// 정수가 입력되고 입력된 숫자 만큼
+// 두 정수를 입력해 합을 출력하는 배열을 생성
+// -주요단어 영문이름 선정
+// 입력 input
+// 정수 decimal
+// 출력 output
+// 합 plus
+// 케이스 case1
+// 배열 arr
+
 public class Main {
+
+    // 처리
+    public int plus(int input_first, int input_second) {
+
+        int output_plus = input_first + input_second;
+
+        return output_plus;
+    }
+
     public static void main(String[] args) {
-        // 문제파악
-        // 유추파악
-        // 주요단어 영문이름 선정
-        // 테스트 케이스
-        // 프로그래밍 순서
-        // try-on
-        // 실패시 원인 기록
+
+        // -테스트 케이스
+        // 3
+        // 2 + 5 = 7
+        // 10 + 20 = 30
+        // 1 + 2 = 3
+
+        // 4
+        // 1 + 2 = 3
+        // 10 + 20 = 30
+        // 20 + 40 = 60
+        // 3 + 5 = 8
+
+        // -프로그래밍 순서
+        // 입력
+        // 처리
+        // 출력
+
+        // 입력
+        Scanner scanner = new Scanner(System.in);
+        int input_case = scanner.nextInt();
+        int arr[] = new int[input_case];
+
+        // 처리
+        for (int i = 0; i < arr.length; i++) {
+            int input_first = scanner.nextInt();
+            int input_second = scanner.nextInt();
+            Main main = new Main();
+            arr[i] = main.plus(input_first, input_second);
+
+        }
+
+        // 출력
+        for (int output : arr)
+            System.out.println(output);
+
+        scanner.close();
+
     }
 
 }
