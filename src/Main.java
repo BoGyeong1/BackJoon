7a370d0c7ea51e03b520d9a691c8f69a5ce3c21a
BoGyeong1 <wls6590@gmail.com>
2022-11-03 14:03:36 +0900

    up programing

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        // 문제파악
+        // 유추파악
+        // 주요단어 영문이름 선정
+        // 테스트 케이스
+        // 프로그래밍 순서
+        // try-on
+        // 실패시 원인 기록
+    }
+
+}
